1c2aafbead137a08ef98e7264bd370c9f015f37f
Mikhail Lens <lenskikh@gmail.com>
2019-12-27 23:59:29 -0500

    Операции с canvas

---
+++ tk_canvas.py
@@ -0,0 +1,41 @@
+'''
+Операции с canvas
+'''
+#-------------------------------------------
+
+#Реакция на клавиатуру
+
+from tkinter import Canvas,Tk,mainloop
+window = Tk()
+
+def callback(event):
+    #перевод события в строку, чтобы можно разделить.
+    razbor = str(event).split(" ")
+    print(razbor)
+
+canvas = Canvas(width=240, height=240, bg="white")
+
+#Установление фокуса на фрейме. Автоматом фокус вне фрейма.
+canvas.focus_set()
+
+#реакция на любую клавишу
+canvas.bind("<Key>", callback)
+canvas.pack()
+mainloop()
+
+#-------------------------------------------
+
+#показывает координаты нажатия мышки
+from tkinter import Canvas,Tk,mainloop
+window = Tk()
+
+def callback(event):
+    print(str(event.x) + "," + str(event.y))
+    
+canvas = Canvas(width=240, height=240, bg="white")
+
+canvas.bind("<Button-1>", callback)
+canvas.pack()
+mainloop()
+
+#-------------------------------------------
