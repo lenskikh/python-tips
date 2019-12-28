52b9ab78b2cfa4e3f5a5883b65bf352ddbddf2fa
Mikhail Lens <lenskikh@gmail.com>
2019-12-27 23:46:51 -0500

    добавлен код, который показывает координаты нажатия от мыши

--- book.py
+++ book.py
@@ -1,6 +1,9 @@
 '''
 Python - поварная книга.
 Пишется для новичков, поэтому код приводится полностью для каждого случая.
+
+* Реакция на клавиатуру
+* Показывает координаты нажатия мышки
 '''
 
 #-------------------------------------------
@@ -22,6 +25,21 @@ canvas.focus_set()
 
 #реакция на любую клавишу
 canvas.bind("<Key>", callback)
+canvas.pack()
+mainloop()
+
+#-------------------------------------------
+
+#показывает координаты нажатия мышки
+from tkinter import Canvas,Tk,mainloop
+window = Tk()
+
+def callback(event):
+    print(str(event.x) + "," + str(event.y))
+    
+canvas = Canvas(width=240, height=240, bg="white")
+
+canvas.bind("<Button-1>", callback)
 canvas.pack()
 mainloop()
 
